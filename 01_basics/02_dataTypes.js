8b16e9b326b87dbb5721f147b80147821aeeed0d
Ayush Mittal <89298670+Ayush-Mittal-17@users.noreply.github.com>
2024-10-19 14:37:16 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treats all js code as the newer version
+
+//alert(3 + 3)  this will run on browser not on node. it will be displayed as a pop up
+
+console.log(3 + 3)
+
+let name = "ayush"
+let age = 21
+let isLoggedIn = false
+
+/*
+Different types of datatypes in js : 
+
+number => range is 2 to the power 53
+bigint
+string => written in double quotes ""
+boolean => true or false
+null => standalone value
+undefined
+symbole => shows uniqueness
+*/
+
+console.log(typeof "ayush");
+
+console.log(typeof undefined);
+
+console.log(typeof null);
