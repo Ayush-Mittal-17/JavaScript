c9ce4435cc8785498cd1263f317fc0b447539682
Ayush Mittal <89298670+Ayush-Mittal-17@users.noreply.github.com>
2025-01-08 14:24:08 +0000

    math and num

--- 01_basics/05_nums_and_maths.js
+++ 01_basics/05_nums_and_maths.js
@@ -5,7 +5,7 @@
 // console.log(balance);
 
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+// console.log(balance.toFixed(2));
 
 // const otherNumber = 123.8966
 // console.log(otherNumber.toPrecision(4));
